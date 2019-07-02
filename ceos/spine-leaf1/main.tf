f5b78999f6a432caafda7d417c1d8d4242f1bb5a
ronmcmilli <ronmcmilli@outlook.com>
2019-07-01 22:00:06 -0500

    Second operational commit

--- ceos/spine-leaf1/main.tf
+++ ceos/spine-leaf1/main.tf
@@ -8,10 +8,11 @@ resource "docker_container" "ceos_test" {
   image = "ceos:latest"
   name = "ceos_test"
   #hostname = "ceos_test"
-  attach = "true"
+  attach = "false"
   env = ["container=docker","CEOS=1","EOS_PLATFORM=ceoslab","SKIP_ZEROTOUCH_BARRIER_IN_SYSDBINIT=1","ETBA=1","INTFTYPE=eth"]
-  command = ["/sbin/init","Cli"]
+  command = ["/sbin/init"]
   privileged = "true"
+  start = "true"
   networks_advanced {
       name = "net1"
   } 
@@ -20,6 +21,9 @@ resource "docker_container" "ceos_test" {
   }
   networks_advanced {
       name = "net3"
+  }
+  networks_advanced {
+      name = "net4"
   }
 }
 
@@ -32,5 +36,8 @@ resource "docker_network" "net2" {
 }
 resource "docker_network" "net3" {
   name = "net3"
+}
+resource "docker_network" "net4" {
+    name = "net4"
 }
 
