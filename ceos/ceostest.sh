40376503f615c105340ac8ee4a9f9adb775b2954
Ron <ronmcmilli@outlook.com>
2020-09-18 20:39:53 -0500

    adding test build script to ceos directory

---
+++ ceos/ceostest.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+docker create --name=ceos1 --privileged -e INTFTYPE=eth -e ETBA=1 -e SKIP_ZEROTOUCH_BARRIER_IN_SYSDBINIT=1 -e CEOS=1 -e EOS_PLATFORM=ceoslab -e container=docker -i -t ceos:latest /sbin/init systemd.setenv=INTFTYPE=eth systemd.setenv=ETBA=1 systemd.setenv=SKIP_ZEROTOUCH_BARRIER_IN_SYSDBINIT=1 systemd.setenv=CEOS=1 systemd.setenv=EOS_PLATFORM=ceoslab systemd.setenv=container=docker
+docker create --name=ceos2 --privileged -e INTFTYPE=eth -e ETBA=1 -e SKIP_ZEROTOUCH_BARRIER_IN_SYSDBINIT=1 -e CEOS=1 -e EOS_PLATFORM=ceoslab -e container=docker -i -t ceos:latest /sbin/init systemd.setenv=INTFTYPE=eth systemd.setenv=ETBA=1 systemd.setenv=SKIP_ZEROTOUCH_BARRIER_IN_SYSDBINIT=1 systemd.setenv=CEOS=1 systemd.setenv=EOS_PLATFORM=ceoslab systemd.setenv=container=docker
+
+# create docker networks
+docker network create net1
+docker network create net2
+
+# connect docker instances to the networks
+docker network connect net1 ceos1
+docker network connect net1 ceos2
+docker network connect net2 ceos1
+docker network connect net2 ceos2
+
+# start the instances
+docker start ceos1
+docker start ceos2
