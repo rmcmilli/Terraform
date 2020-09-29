4ac495c7be0208cd265ec7c82f7ed4dcb859565c
Ron <ronmcmilli@outlook.com>
2020-09-29 15:40:14 -0500

    -redistribute connected leaf-1 + host 2

--- ceos/5-node_portchannel_ospf-vxlan/5-node-topology_port-channel.tf
+++ ceos/5-node_portchannel_ospf-vxlan/5-node-topology_port-channel.tf
@@ -198,7 +198,6 @@ resource "docker_container" "leaf-3" {
 }
 
 resource "docker_container" "host1_leaf1" {
-  #image = "ubuntu:18.04"
   # The image below is alpine-based with installed network tools
   image    = docker_image.network-multitool.latest
   name     = "host1_leaf1"
@@ -215,6 +214,28 @@ resource "docker_container" "host1_leaf1" {
   #restart = "always"
   networks_advanced {
     name = docker_network.eth10.name
+  }
+  capabilities {
+    add = ["NET_ADMIN"]
+  }
+}
+resource "docker_container" "host2_leaf1" {
+  # The image below is alpine-based with installed network tools
+  image    = docker_image.network-multitool.latest
+  name     = "host2_leaf1"
+  hostname = "host2_leaf1"
+  attach   = "false"
+  logs     = "false"
+  command = ["./host2-int.sh"]
+  upload {
+    source = "host2-int.sh"
+    file = "host2-int.sh"
+    executable = "true"
+  }
+  start   = "true"
+  #restart = "always"
+  networks_advanced {
+    name = docker_network.eth11.name
   }
   capabilities {
     add = ["NET_ADMIN"]
---
+++ ceos/5-node_portchannel_ospf-vxlan/host2-int.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ip addr add 10.0.20.2/24 dev eth0
+ip route delete default
+ip route add default via 10.0.20.1 dev eth0
+#This makes it so the container doesn't stop
+tail -f /dev/null
